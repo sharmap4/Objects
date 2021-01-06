364ce9eef533510dceccd11761834959fce223dd
sharmap4 <sharmapulkitmukesh@gmail.com>
2021-01-06 14:02:26 +0000

    ben

---
+++ js3.js
@@ -0,0 +1,9 @@
+let desiredPleasantry = "greeting";
+
+let bensPhrases = {
+  greeting: "Well hello there!",
+  farewell: "See you in another life brother",
+  smallTalk: "Warm today isn't it...",
+};
+
+console.log(bensPhrases.greeting)
