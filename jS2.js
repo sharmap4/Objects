7518e9130a7c5fd6b02b00082092eef0aa03a5e9
sharmap4 <sharmapulkitmukesh@gmail.com>
2021-01-06 13:12:25 +0000

    Objects js

---
+++ jS2.js
@@ -0,0 +1,8 @@
+let person = {
+    name: "Ben",
+    likesChocolate: true,
+};
+
+if (likesCholate === true) {
+    console.log ("Ben loves chocolate");
+} else {console.log  ("Ben hates chocolate");}
---
+++ main.js
@@ -0,0 +1,20 @@
+let myHouse = {
+    Stories: 2,
+    Parking: "no",
+    Bedrooms: 2,
+    Garden: "yes",
+    energyEfficiency: "D",
+};
+
+let myCar = {
+    wheels: 4,
+    powerSteering: "yes",
+    seats: 2,
+    millage: "20mpg"
+};
+
+let favoriteMovie = {
+    name: "The Dark Knight",
+    releaseYear: 2008,
+    leadActor: "Christian Bale"
+};
